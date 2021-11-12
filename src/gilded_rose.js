926adf54ad57c816fb53ab589977d5b7e3dfdbe3
William Rodil <william.rodil@watsapsap.com>
2021-11-12 12:54:44 +0000

    Added test for Sulfaras

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,12 +11,18 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    const maxItem = 50;
+    let maxItem = 50;
+    const sulfurasQuality = 80;
+
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
+          } else {
+            //"Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+            this.items[i].sellIn = this.items[i].sellIn - 1;
+            this.items[i].quality = sulfurasQuality;
           }
         }
       } else {
@@ -24,12 +30,12 @@ class Shop {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
+              if (this.items[i].quality < maxItem) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
+              if (this.items[i].quality < maxItem) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
@@ -51,16 +57,19 @@ class Shop {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
+          if (this.items[i].quality < maxItem) {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
+
       // - "Conjured" items degrade in Quality twice as fast as normal items
       if (this.items[i].name.includes('Conjured')) {
         this.items[i].quality = this.items[i].quality - 2;
       } 
+
     }
+    
 
     return this.items;
   }
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,17 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+  it("should return the sellIn and quality for Conjured items", function() {
+    const gildedRoseConjured = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+    const items = gildedRoseConjured.updateQuality();
+    expect(items[0].sellIn).toBe(2);
+    expect(items[0].quality).toBe(3);
+  });
+  it("should return the right sellIn and quality for Sulfuras", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 6)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("Conjured Mana Cake");
+    console.log((items));
+    expect(items[0].sellIn).toBe(-2);
+    expect(items[0].quality).toBe(80);
   });
 });
